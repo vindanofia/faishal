1270e5d5971282c6c3a96215dd8f0b67f751c812
vindanofia <vindanofia14@gmail.com>
2020-07-08 17:00:14 +0700

    update logo

--- application/views/index.php
+++ application/views/index.php
@@ -10,7 +10,7 @@
 	<meta content="" name="keywords">
 
 	<!-- Favicons -->
-	<link href="<?= base_url() ?>flexor/assets/img/favicon.png" rel="icon">
+	<link href="<?= base_url() ?>flexor/assets/img/K3_kecil.png" rel="icon">
 	<link href="<?= base_url() ?>flexor/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
 	<!-- Google Fonts -->
@@ -59,7 +59,7 @@
 		<div class="container d-flex">
 
 			<div class="logo mr-auto">
-				<h1 class="text-light"><a href="index.html"><span>EFIMA</span></a></h1>
+				<h1 class="text-light"><a href="<?= base_url() ?>"><span>EFIMA</span></a></h1>
 				<!-- Uncomment below if you prefer to use an image logo -->
 				<!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 			</div>
